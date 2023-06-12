f8c61396999f1bde8c0bf3b5a665241089db4df7
Kaikyl <kaikylimasouza@gmail.com>
2023-06-12 20:21:47 -0300

    tde 12-06

---
+++ tde 12-06-23/tde120623/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Cabeca from './components/Cabeca';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Cabeca/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tde 12-06-23/tde120623/src/components/Cabeca.js
@@ -0,0 +1,12 @@
+import './Cabeca.css'
+
+const cabeca = () => {
+    return (
+        <div className="cabecaHeader">
+            <p className='cabecaText'>Header</p>
+
+        </div>
+    );
+}
+
+export default cabeca;
