6d181993701b6701a757bbec6e60ab0369e097a1
Kaikyl <kaikylimasouza@gmail.com>
2023-06-26 20:25:21 -0300

    update

--- trabalho/trabalho/src/pages/Detalhes.jsx
+++ trabalho/trabalho/src/pages/Detalhes.jsx
@@ -11,7 +11,7 @@ const Detalhes = () => {
     const fetchPokeData = useCallback (async (paramOffset) =>{
         console.log('teste')
         try{
-            const{data} = await axios.get(`https://restcountries.com/v3.1/name/${name}`)
+            const{data} = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
             setPokeData(data)
             console.log(data)
         }catch(error){
@@ -32,12 +32,12 @@ const Detalhes = () => {
 
     const renderPokeData = () => {
         return (
-            <div className="container">
+            <div className="containerr">
                 {pokeData.map(poke =>(
-                    <div className=".container">
+                    <div className="containerr">
                         <h4>Nome: {poke.name.common}</h4>
-                        <img src= {poke.flags.png} alt="" />
-                        <h4>Capital: {poke.capital[0]}</h4>
+                        <img src= {poke.flags.png} alt="" className="bandeira"/>
+                        {poke.capital?<h4>Capital: {poke.capital[0]}</h4>:""}
                         <h4>Continete: {poke.continents[0]}</h4>
                         <h4>População: {poke.population}</h4>
                         <h4>Horário local: {poke.timezones[0]}</h4>
